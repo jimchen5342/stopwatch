23f08b066fa169cd8fe8620e089173cc71ca174b
jimchen5342 <jimchen5342@gmail.com>
2025-06-19 08:38:58 +0000

    2025-06-19 17:00

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = 1 // flutter.versionCode
-        versionName = "2025-06-17 17:30" // flutter.versionName
+        versionName = "2025-06-19 17:00" // flutter.versionName
         setProperty("archivesBaseName", "stopwatch")
     }
 
